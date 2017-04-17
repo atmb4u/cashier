1dc62886bb8c831dcb9c553290641e080e760075
elishaROBINSON <elisha.rob@gmail.com>
2017-04-17 13:07:45 +0530

    horrible indent

--- unit_test.py
+++ unit_test.py
@@ -3,42 +3,41 @@ from hashlib import md5
 from cachier import Cashier ,cache
 
 class cashier_tests(unittest.TestCases):
-  """class for running unit tests on Cashier"""
+        """class for running unit tests on Cashier"""
+  	def __init__(self):
+    		self.cache_inst = Cashier(file_name="unit_testing")
+    		self.cache_inst.clear()
   
-  def __init__(self):
-    self.cache_inst = Cashier(file_name="unit_testing")
-    self.cache_inst.clear()
-  
-  def test_CRUD_operation(self):
-    """objective :to perform CRUD operations using cashier"""    
+        def test_CRUD_operation(self):
+  	    """objective :to perform CRUD operations using cashier"""    
     
-    test_set_1=['test_string_1','special_char_str ^@#$%&#*#%$^',\
+   	    test_set_1=['test_string_1','special_char_str ^@#$%&#*#%$^',\
               "big string jksaldjflkjlkjgn,zmnc,vmn,msnfm,mn,mn"]
-    test_set_2=['test_string_2','special_char_str^@#$%&#*#%$^',\
+   	    test_set_2=['test_string_2','special_char_str^@#$%&#*#%$^',\
               "big string jksaldjflkjlkjgn,zmnc,mn,msnfm,mn,mn"]
-    try :
+            try :
 		  """create and read operations"""
 		  for val in test_set_1 :
 			  key=md5(val.encode('utf8')).hexdigest()
 			  self.cache_inst.set(key,val)
 			  assert("missing value ",self.cache_inst.get(key) not in test_set_1)
         
-  	  """update operation"""
+  	          """update operation"""
 		  for val in test_set_1:
 			  index_val=test_set_1.index(val)
 			  key=md5(val.encode('utf8')).hexdigest()
 			  self.cache_inst.set(key,test_set_2[index_val])
 			  assert("missing value",self.cache_inst.get(key) not in test_set_2)
         
-	  	"""delete operation """
-	  	for val in test_set_1:
+	  	  """delete operation """
+	  	  for val in test_set_1:
 	  		key=md5(val.encode('utf8')).hexdigest()
 	  		self.cache_inst.delete(key)
         
-    except Exception as e:
-      print("something went wrong :",str(e))
+    	    except Exception as e:
+      			print("something went wrong :",str(e))
       
-     
+
 if __name__ =="__main__" :
   unittest.main()
   
